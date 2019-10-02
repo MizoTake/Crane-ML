09c3cc9cadf24f9959ef7986fc6ebd60db432e42
MizoTake <mizotake123@gmail.com>
2019-10-03 01:34:09 +0900

    first

---
+++ Assets/Crane-ML/Script/CraneGamePresenter.cs
@@ -0,0 +1,40 @@
+﻿using DG.Tweening;
+using Doozy.Engine.UI;
+using UniRx;
+using UnityEngine;
+
+namespace CranML
+{
+    public class CraneGamePresenter : MonoBehaviour
+    {
+
+        [SerializeField] private UIButton leftButton;
+        [SerializeField] private UIButton forwardButton;
+        [SerializeField] private Transform arm;
+        [SerializeField] private Animator armAnim;
+        
+        void Start()
+        {
+            Bind();
+        }
+
+        private void Bind()
+        {
+            // TODO: 初期値からの移動場所
+            
+            leftButton.Button.OnClickAsObservable()
+                .TakeUntilDestroy(this)
+                .Subscribe(_ =>
+                {
+                    arm.DOMoveX(arm.transform.position.x + 0.01f, 0.1f);
+                });
+            
+            forwardButton.Button.OnClickAsObservable()
+                .TakeUntilDestroy(this)
+                .Subscribe(_ =>
+                {
+                    arm.DOMoveZ(arm.transform.position.z + 0.01f, 0.1f);
+                });
+        }
+    }
+}
